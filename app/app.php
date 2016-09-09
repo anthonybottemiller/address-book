804ed934c9d5c5dc565546a5f71e3955851dab36
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-09-09 14:52:36 -0700

    adds code that creates a new contact utilizing user input

--- app/app.php
+++ app/app.php
@@ -1,6 +1,11 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Contact.php";
+    session_start();
+    if (empty($_SESSION['contacts'])) {
+    $_SESSION['contacts'] = array();
+    }
 
     $app = new Silex\Application();
     
@@ -10,6 +15,12 @@
 
     $app->get("/", function() use ($app) {
       return $app['twig']->render('address-book.html.twig');
+    });
+
+    $app->get("/add-contact", function() use ($app) {
+      $new_contact = new Contact($_GET['name'], $_GET['address'], $_GET['phone-number']);
+      $new_contact->save();
+      return $_SESSION['contacts'][0]->name;
     });
     return $app;
 ?>
--- src/Contact.php
+++ src/Contact.php
@@ -12,5 +12,12 @@
         $this->address = $address;
         $this->phone_number = $phone_number;
       }
-
+      function save()
+      {
+        array_push($_SESSION['contacts'], $this);
+      }
+      static function getAll()
+      {
+        return $_SESSION['list_of_tasks'];
+      }
     }
