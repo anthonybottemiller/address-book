f4faf5ef59a0a4dbeae6643623d6a624147bad75
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-09-09 17:45:33 -0700

    changes 'Contact' to have getters and setters instead of public properties

--- src/Contact.php
+++ src/Contact.php
@@ -1,9 +1,9 @@
 <?php
     class Contact
     {
-      public $name;
-      public $address;
-      public $phone_number;
+      private $name;
+      private $address;
+      private $phone_number;
 
 
       function __construct($name, $address, $phone_number)
@@ -12,14 +12,47 @@
         $this->address = $address;
         $this->phone_number = $phone_number;
       }
+
+      function setName($name)
+      {
+        $this->name = $name;
+      }
+
+      function getName()
+      {
+        return $this->name;
+      }
+
+      function setAddress($address)
+      {
+        $this->address = $address;
+      }
+
+      function getAddress()
+      {
+        return $this->address;
+      }
+
+      function setPhone($phone_number)
+      {
+        $this->phone_number = $phone_number;
+      }
+
+      function getPhone()
+      {
+        return $this->phone_number;
+      }
+
       function save()
       {
         array_push($_SESSION['contacts'], $this);
       }
+
       static function getAll()
       {
         return $_SESSION['contacts'];
       }
+
       static function deleteAll()
       {
         $_SESSION['contacts'] = array();
