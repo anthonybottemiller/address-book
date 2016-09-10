332d18c01c95c74604020223a1fef31e6b98356d
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-09-09 17:06:35 -0700

    fixed html formatting and adds confirmation pages for actions with link buttons home

--- app/app.php
+++ app/app.php
@@ -21,12 +21,12 @@ error_reporting(E_ALL);
     $app->post("/add-contact", function() use ($app) {
       $new_contact = new Contact($_POST['name'], $_POST['address'], $_POST['phone-number']);
       $new_contact->save();
-      return $app['twig']->render('address-book.html.twig', array('contacts' => Contact::getAll()));
+      return $app['twig']->render('new-contact.html.twig');
     });
 
     $app->post("/delete-all", function() use ($app) {
       Contact::deleteAll();
-      return $app['twig']->render('address-book.html.twig', array('contacts' => Contact::getAll()));
+      return $app['twig']->render('delete-contacts.html.twig', array('contacts' => Contact::getAll()));
     });
     return $app;
 ?>
